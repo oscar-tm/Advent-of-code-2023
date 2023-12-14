0feaf69f9254be1dba14d534c1a8a57f37c80a0e
Oscar Tegnér Mohringe <58402563+oscar-tm@users.noreply.github.com>
2023-12-14 15:24:09 +0100

    Day 14

---
+++ Day 14/main.py
@@ -0,0 +1,71 @@
+dish = []
+
+with open('g:/Programmering/Python/Advent of code 2023/Day 14/input.txt', 'r') as file:
+    for line in file:
+        dish.append(list(line.strip()))
+
+mapDict = {'.' : 0, '#' : 1, 'O' : 2}
+for i in range(len(dish)):
+    for j in range(len(dish[i])):
+        dish[i][j] = mapDict[dish[i][j]]
+
+def north(dish):
+    for j in range(len(dish[0])):
+        for i in range(len(dish)):
+            if dish[i][j] == 2:
+                c = 1
+                while i-c >= 0 and dish[i-c][j] == 0:
+                    dish[i-c][j] = 2
+                    dish[i-c+1][j] = 0
+                    c += 1
+    return dish
+
+def south(dish):
+    dish = dish[::-1]
+    dish = north(dish)
+    dish = dish[::-1]
+    return dish
+
+def east(dish):
+    dish = [list(x) for x in zip(*dish)]
+    dish = south(dish)
+    dish = [list(x) for x in zip(*dish)]
+    return dish
+
+def west(dish):
+    dish = [list(x) for x in zip(*dish)]
+    dish = north(dish)
+    dish = [list(x) for x in zip(*dish)]
+    return dish
+
+def load(dish):
+    ans = 0
+    for i in range(len(dish)):
+        for j in range(len(dish[0])):
+            if dish[i][j] == 2:
+                ans += len(dish) - i
+    return ans
+
+import time
+s = time.time()
+cState = tuple(tuple(x) for x in dish)
+seen = {}
+loads = [0]
+i = 1
+while cState not in seen:
+    seen[cState] = i
+
+    dish = north(dish)
+    dish = west(dish)
+    dish = south(dish)
+    dish = east(dish)
+    loads.append(load(dish))
+
+    cState = tuple(tuple(x) for x in dish)
+    i += 1
+
+cycleLen = i - seen[cState]
+shift = seen[cState]
+
+print(loads[shift + (1000000000 - shift) % cycleLen])
+print(time.time() - s)
